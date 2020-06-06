e481a1eadfaecc898fdeece6fec4c429a011665c
nakuljhunjhunwala <jhunjhunwalanakul@gmail.com>
2020-06-07 03:06:29 +0530

    Update Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -33,7 +33,7 @@ export const fetchNews = async ()=> {
         const date = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()-1}`;
         
 const proxyUrl = "https://cors-anywhere.herokuapp.com/"
-const url = `${proxyUrl}https://newsapi.org/v2/everything?q=covid%2019&from=${date}&pageSize=6&language=en&domains=ndtv.com,indiatoday.in,indianexpress.com&sortBy=popularity&apiKey=8067ce9c47be446085bc0e70056d7665`;
+const url = `${proxyUrl}http://newsapi.org/v2/everything?q=covid%2019&from=${date}&pageSize=6&language=en&domains=ndtv.com,indiatoday.in,indianexpress.com&sortBy=popularity&apiKey=8067ce9c47be446085bc0e70056d7665`;
 
 
         // const url = `http://newsapi.org/v2/everything?q=covid%2019&from=${date}&pageSize=6&language=en&domains=ndtv.com,indiatoday.in,indianexpress.com&sortBy=popularity&apiKey=8067ce9c47be446085bc0e70056d7665`;
