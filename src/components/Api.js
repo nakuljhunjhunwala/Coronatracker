85704aa03cb5927dbf11e4487c5005831da1ddcb
unknown <jhunjhunwalanakul@gmail.com>
2020-06-07 02:12:40 +0530

    clean up

--- src/components/Api.js
+++ src/components/Api.js
@@ -32,7 +32,7 @@ export const fetchNews = async ()=> {
         const d = new Date();
         const date = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()-1}`;
 
-        const url = `http://newsapi.org/v2/everything?q=covid%2019&from=${date}&pageSize=6&language=en&domains=ndtv.com,indiatoday.in,indianexpress.com&sortBy=popularity&apiKey=8067ce9c47be446085bc0e70056d7665`;
+        const url = `https://newsapi.org/v2/everything?q=covid%2019&from=${date}&pageSize=6&language=en&domains=ndtv.com,indiatoday.in,indianexpress.com&sortBy=popularity&apiKey=8067ce9c47be446085bc0e70056d7665`;
         const {data} = await axios.get(url);
 
         return data;
