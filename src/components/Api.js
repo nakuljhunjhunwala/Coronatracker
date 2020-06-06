38c419555f23cb91b0f3bdf3aa973949187982bc
nakuljhunjhunwala <jhunjhunwalanakul@gmail.com>
2020-06-07 03:01:23 +0530

    clean up 2

--- src/components/Api.js
+++ src/components/Api.js
@@ -29,13 +29,18 @@ export const fetchZone = async ()=> {
 export const fetchNews = async ()=> {
     try {
 
-        const d = new Date();
+              const d = new Date();
         const date = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()-1}`;
+        
+const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+const url = `${proxyUrl}https://newsapi.org/v2/everything?q=covid%2019&from=${date}&pageSize=6&language=en&domains=ndtv.com,indiatoday.in,indianexpress.com&sortBy=popularity&apiKey=8067ce9c47be446085bc0e70056d7665`;
 
-        const url = `https://newsapi.org/v2/everything?q=covid%2019&from=${date}&pageSize=6&language=en&domains=ndtv.com,indiatoday.in,indianexpress.com&sortBy=popularity&apiKey=8067ce9c47be446085bc0e70056d7665`;
+
+        // const url = `https://newsapi.org/v2/everything?q=covid%2019&from=${date}&pageSize=6&language=en&domains=ndtv.com,indiatoday.in,indianexpress.com&sortBy=popularity&apiKey=8067ce9c47be446085bc0e70056d7665`;
         const {data} = await axios.get(url);
 
         return data;
+
     } catch (error) {
         alert(error);
     }
